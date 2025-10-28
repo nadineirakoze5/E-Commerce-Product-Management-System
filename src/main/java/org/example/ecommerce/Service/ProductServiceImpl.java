f271d6aa92818903c0c96178a86dae88348c205e
nadineirakoze5 <nirakoze72@gmail.com>
2025-10-28 09:54:23 +0200

    removed application.properties

--- src/main/java/org/example/ecommerce/Service/ProductServiceImpl.java
+++ src/main/java/org/example/ecommerce/Service/ProductServiceImpl.java
@@ -9,7 +9,6 @@ import org.example.ecommerce.Repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
